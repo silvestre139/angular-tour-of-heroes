3f8f480a5ba7130e1caf5bb16f3d89a4f0443ce2
Jose Silvestre Rodriguez Ortiz <43645665+silvestre139@users.noreply.github.com>
2024-02-29 07:06:11 -0500

    Added products to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Products, Product } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productService: ProductsService) {}
+  products: Product[] = [];
   ngOnInit() {
     this.productService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
         console.log(products.items);
+        this.products = products.items;
       });
   }
 }
